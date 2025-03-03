8fffebcdf2d910a42d6c166d6de4dbda9c1875b8
dcbritton <122560490+dcbritton@users.noreply.github.com>
2025-03-03 14:57:42 -0500

    Fix encoding issue on data processing python script

--- data/process.py
+++ data/process.py
@@ -2,14 +2,14 @@
 
 import csv
 
-infile = open('Lexique383.csv', 'r')
+infile = open('Lexique383.csv', 'r', encoding='utf-8-sig')
 outfile = open('output.csv', 'w')
 csvDict = csv.DictReader(infile)
 
 outfile.write("mot,genre,freq\n")
 for line in csvDict:
     if line['4_cgram'] == 'NOM' and (line['6_nombre'] == 's' or line['6_nombre'] == ''):
-        outfile.write(f"{line['ï»¿1_ortho']},{line['5_genre']},{line['10_freqlivres']}\n")
+        outfile.write(f"{line['1_ortho']},{line['5_genre']},{line['10_freqlivres']}\n")
 
 outfile.close()
 infile.close()
