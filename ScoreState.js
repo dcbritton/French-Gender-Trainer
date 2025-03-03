2cec2a0d35843d18b83bab30b9640f9cf991666c
dcbritton <122560490+dcbritton@users.noreply.github.com>
2025-03-03 02:38:08 -0500

    Refactor ScoreState for information hiding

--- ScoreState.js
+++ ScoreState.js
@@ -2,29 +2,30 @@
 // contains a class to keep track of the current session's score, incorrect words, etc.
 
 class ScoreState {
+
+    #total = 0
+    #numCorrect = 0
+    #missedWords = []
+
     // constructor
-    constructor() {
-        this.total = 0
-        this.numCorrect = 0
-        this.missedWords = []
-    }
+    constructor() {}
 
     //
     init() {
-        document.getElementById("current-score").innerText = `${this.numCorrect}/${this.total}`
+        document.getElementById("current-score").innerText = `${this.#numCorrect}/${this.#total}`
     }
 
     // 
     check(response) {
         // incorrect response
         if (response != wordState.getCurrentGender()) {
-            this.missedWords.push([wordState.getCurrentWord(), wordState.getCurrentGender()])
+            this.#missedWords.push([wordState.getCurrentWord(), wordState.getCurrentGender()])
         }
         else {
-            this.numCorrect += 1
+            this.#numCorrect += 1
         }
 
-        this.total += 1
-        document.getElementById("current-score").innerText = `${this.numCorrect}/${this.total}`
+        this.#total += 1
+        document.getElementById("current-score").innerText = `${this.#numCorrect}/${this.#total}`
     }
 }
