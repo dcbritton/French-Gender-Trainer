e87c72065770190a8595867e3ab062b3bdccdee7
dcbritton <122560490+dcbritton@users.noreply.github.com>
2025-03-03 00:31:40 -0500

    Replace magic numbers in WordState with consts

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // this file contains scripts for the front-end
 
+//
 function getTestWords() {
     return [
         ["temps", "m"],
@@ -9,6 +10,12 @@ function getTestWords() {
     ]
 }
 
+// declare constants for WordState
+const WORD_INDEX = 0
+const GENDER_INDEX = 1
+const CURRENT_WORD_INDEX = 0
+
+// tracks the state of current and following words. responsible for getting next set of words
 class WordState {
     constructor(getNextWordSet) {
         this.wordBuffer = []
@@ -21,15 +28,15 @@ class WordState {
 
     init() {
         this.refillBuffer()
-        this.currentWord = this.wordBuffer[0][0]
-        this.currentGender = this.wordBuffer[0][1]
+        this.currentWord = this.wordBuffer[CURRENT_WORD_INDEX][WORD_INDEX]
+        this.currentGender = this.wordBuffer[CURRENT_WORD_INDEX][GENDER_INDEX]
         document.getElementById("current-word").innerText = this.currentWord
     }
 
     getNextWord() {
         this.wordBuffer.shift()
-        this.currentWord = this.wordBuffer[0][0]
-        this.currentGender = this.wordBuffer[0][1]
+        this.currentWord = this.wordBuffer[CURRENT_WORD_INDEX][WORD_INDEX]
+        this.currentGender = this.wordBuffer[CURRENT_WORD_INDEX][GENDER_INDEX]
         document.getElementById("current-word").innerText = this.currentWord
     }
 }
